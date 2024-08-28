2b8b084cca5c7b5d96c2ce14ce73fbbf44503606
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-08-28 23:25:26 +0545

    closer look at functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
-// 
+/////////////////////////////
+// 3. Default Parameters 
 /*
 const bookings = []
 
@@ -19,6 +20,41 @@ createBooking('LH123')
 createBooking('LH123', 20, 40)
 */
 
+///////////////////////////////////////
+/*
+// How Passing Arguments Works: Values vs. Reference
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  passport: 24739479284,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 24739479284) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// Is the same as doing...
+// const flightNum = flight;
+// const passenger = jonas;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
+*/
 
 //5. First Class Funtions vs High Order Functions
 
