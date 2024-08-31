05ca07f6c30b7205dff1bd9f8d7855b8084e6d7c
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-08-31 14:34:51 +0545

    filter method example

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -101,7 +101,7 @@ console.log(accounts)
 //   ['GBP', 'Pound sterling'],
 // ]);
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 /*
@@ -184,3 +184,16 @@ const movementsDescriptions = movements.map(
 );
 console.log(movementsDescriptions);
 */
+
+// Filter exmaple
+const deposits = movements.filter(function(mov) {
+  return mov > 0;
+})
+console.log(movements)
+console.log(deposits)
+
+// const depositFor = [];
+// for (const mov of movements) if (mov > 0) depositFor.push(mov);
+// console.log(depositFor)
+
+const withdrawals = movements.filter(mov => mov < 0);
