836da53c096023f233c24639d9e45a1129851025
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-08-31 17:11:07 +0545

    calc average using method chaining

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -255,15 +255,22 @@ const calcAverageHumanAge = ages => {
   const average = adults.reduce((acc, age, i, arr) => acc + age / arr.length,0)
   return average;
 }
+
+
 const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3])
 const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 console.log(avg1, avg2);
 */
-
-
+// 17. Challenge 3 (using method chaining)
+const calcAverageHumanAge = ages => ages.map(age => age <= 2? 2 * age: 16 + age * 4).filter(age => age >= 18).reduce((acc, age, i, arr) => acc + age / arr.length,0);
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3])
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+console.log(avg1, avg2);
+/*
 /// 16. method chaining
 const eurToUsd = 1.1;
 
 //PIPELINE
 const totalDepositsUSD = movements.filter(mov => mov > 0). map(mov => mov * eurToUsd).reduce((acc, mov) => acc + mov, 0);
 console.log(totalDepositsUSD)
+*/
