c6e2317fdfad2b8d130bbfba82462d924e6028ee
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-08-31 13:21:18 +0545

    array challenge 1

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -85,16 +85,16 @@ displayMovements(account1.movements)
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
-
+/*
 /////////////////////////////////////////////////
 // Simple Array Methods
 let arr = ['a', 'b', 'c', 'd', 'e'];
@@ -129,3 +129,24 @@ console.log([...arr, ...arr2]);
 // JOIN
 console.log(letters.join(' - '));
 
+*/
+//Coding Challenge
+const checkDogs = function(dogsJulia, dogsKate) {
+  const dogsJuliaCorrected = dogsJulia.slice()
+  dogsJuliaCorrected.splice(0,1)
+  dogsJuliaCorrected.splice(-2)
+  console.log(dogsJuliaCorrected)
+
+  const dogs = dogsJuliaCorrected.concat(dogsKate)
+
+  dogs.forEach(function(dog, i) {
+    if (dog >= 3) {
+      console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old`);
+    } else {
+      console.log(`Dog number ${i + 1} is still a puppy 🐶`);
+    }
+  })
+}
+
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+// checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
