66047e784cdf0f200f5a0ba1fb40d086f4d5072b
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-09-01 13:49:25 +0545

    code refactor till login

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -61,33 +61,63 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function(movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = '';
-  
-  movements.forEach(function (mov, i) {
-    const type = mov > 0? 'deposit' : 'withdrawal';
+
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const html = `
       <div class="movements__row">
-          <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
-          <div class="movements__value">${mov}€</div>
-        </div>
-    `
-    
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+        <div class="movements__value">${mov}€</div>
+      </div>
+    `;
+
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
+};
+// console.log(containerMovements.innerHTML)
+
+const calcDisplayBalance = function (currentAccount) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0)
+  labelBalance.textContent = `${balance}€`
 }
 
-// console.log(containerMovements.innerHTML)
+const calcDisplaySummary = function (acc) {
+  const incomes = acc.movements.filter(mov => mov > 0).reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}€`
+
+  const out = acc.movements.filter(mov => mov < 0).reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)}€`
+
+  const interest = acc.movements.filter(mov => mov > 0).map(deposit => deposit * acc.interestRate/100).filter((int, i, arr) => {return int >= 1})
+      .reduce((acc,int) => acc + int, 0)
+  labelSumInterest.textContent = `${interest}€`
+}
 
 const createUsernames = function(accs) {
   accs.forEach (function(acc) {
     acc.username = acc.owner.toLowerCase().split(' ').map(name => name[0]).join('');
   })
 }
-
 createUsernames(accounts)
 // console.log(username)
+
+const updateUI = function (acc) {
+  // Display movements
+  displayMovements(acc.movements);
+
+  // Display balance
+  calcDisplayBalance(acc);
+
+  // Display summary
+  calcDisplaySummary(acc);
+};
 
 let currentAccount;
 //Event handler
@@ -103,35 +133,16 @@ btnLogin.addEventListener('click', function(e) {
 
     //clear input field
     inputLoginUsername.value = inputLoginPin.value = '';
-    inputLoginPin.blur();
+    inputLoginPin.blur(); // to remove the focus from input field
 
-    //display movement
-    displayMovements(account1.movements)
-
-    //display balance
-    calcDisplayBalance(account1.movements);
-
-    //display summary
-    calcDisplaySummary(account1.movements)
+    //update ui
+    updateUI(currentAccount);
   };
 })
 
-const calcDisplayBalance = function (currentAccount) {
-  const balance = movements.reduce((acc, mov) => acc + mov, 0)
-  labelBalance.textContent = `${balance} EUR`
-}
 
-const calcDisplaySummary = function (acc) {
-  const incomes = acc.movements.filter(mov => mov > 0).reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`
 
-  const out = acc.movements.filter(mov => mov < 0).reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`
 
-  const interest = acc.movements.filter(mov => mov > 0).map(deposit => deposit * acc.interestRate/100).filter((int, i, arr) => {return int >= 1})
-      .reduce((acc,mov) => acc + mov, 0)
-  labelSumInterest.textContent = `${interest}`
-}
 
 
 
