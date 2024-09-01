4772dbe4ed56ac2e68114d44e26137b46454b07f
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-09-01 13:23:54 +0545

    find method example

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -302,3 +302,14 @@ const eurToUsd = 1.1;
 const totalDepositsUSD = movements.filter(mov => mov > 0). map(mov => mov * eurToUsd).reduce((acc, mov) => acc + mov, 0);
 console.log(totalDepositsUSD)
 */
+
+/*
+// 18. find method
+const firstWithdrawal = movements.find(mov => mov < 0);
+console.log(movements);
+console.log(firstWithdrawal);
+
+console.log(accounts);
+const account = accounts.find(acc => acc.owner === 'Jessica Davis')
+console.log(account);
+*/
