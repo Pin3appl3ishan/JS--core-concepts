70fbccbe2c134fd104893ba3ae4ea1ca0963c209
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-09-02 19:35:44 +0545

    mapping exception

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -433,6 +433,7 @@ console.log(++a);
 console.log(a);
 */
 
+/*
 // 3. using objects as accumulator
 const { deposits, withdrawals } = accounts
   .flatMap(acc => acc.movements)
@@ -446,3 +447,25 @@ const { deposits, withdrawals } = accounts
   );
 
 console.log(deposits, withdrawals);
+*/
+
+
+// 4.
+// this is a nice title -> This Is a Nice Title
+const convertTitleCase = function (title) {
+  const capitzalize = str => str[0].toUpperCase() + str.slice(1);
+
+  const exceptions = ['a', 'an', 'and', 'the', 'but', 'or', 'on', 'in', 'with'];
+
+  const titleCase = title
+    .toLowerCase()
+    .split(' ')
+    .map(word => (exceptions.includes(word) ? word : capitzalize(word)))
+    .join(' ');
+
+  return capitzalize(titleCase);
+};
+
+console.log(convertTitleCase('this is a nice title'));
+console.log(convertTitleCase('this is a LONG title but not too long'));
+console.log(convertTitleCase('and here is another title with an EXAMPLE'));
