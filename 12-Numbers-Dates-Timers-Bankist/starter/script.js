231a58f93400dbdb5ab421287a37bf743070c95d
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-09-03 07:06:08 +0545

    setTimeout example

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -281,3 +281,9 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+const ingredients = ['olives', 'spinach']
+const pizzaTimer = setTimeout((ing1, ing2) => console.log(`Here is your pizza with ${ing1} & ${ing2}`), 3000, ...ingredients)
+
+console.log('Waiting...')
+
+if (ingredients.includes('spinach')) clearTimeout(pizzaTimer);
