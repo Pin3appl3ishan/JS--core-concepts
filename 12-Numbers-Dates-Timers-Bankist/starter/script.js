1d9b12fe63ebecf1e5499c8663f8566cb95f964c
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-09-03 14:03:13 +0545

    SetInterval example

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -283,9 +283,17 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+/*
 const ingredients = ['olives', 'spinach']
 const pizzaTimer = setTimeout((ing1, ing2) => console.log(`Here is your pizza with ${ing1} & ${ing2}`), 3000, ...ingredients)
 
 console.log('Waiting...')
 
 if (ingredients.includes('spinach')) clearTimeout(pizzaTimer);
+*/
+
+// SetInterval
+setInterval(function() {
+  const now = new Date();
+  console.log(now);
+}, 1000)
