07a4a30ec7d228b16fc170c0bbacb53ac7298134
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-09-05 23:35:36 +0545

    Event delegation

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -71,6 +71,19 @@ btnScrollTo.addEventListener('click', function (e) {
 //   });
 // });
 
+// 1. Add event listener to common parent element
+// 2. Determine what element originated the event
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
 /*
 ///////////////////////////////////////
 // Selecting, Creating, and Deleting Elements
