b79c5469e3de1558b0c476b90eeedfeb0764a5d8
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-09-05 23:42:00 +0545

    DOM

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -109,6 +109,11 @@ console.log(h1.nextElementSibling);
 console.log(h1.previousSibling);
 console.log(h1.nextSibling);
 
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(function (el) {
+  if (el !== h1) el.style.transform = 'scale(0.5)';
+});
+
 /*
 ///////////////////////////////////////
 // Selecting, Creating, and Deleting Elements
