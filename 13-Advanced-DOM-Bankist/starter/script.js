649d741f2b4d5954dadaaa5b2b7faeedc3145128
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-09-05 23:40:29 +0545

    DOM traversal

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -84,6 +84,16 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
   }
 });
 
+// DOM Traversing
+const h1 = document.querySelector('h1');
+
+// Going downwards: child
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered';
+
 /*
 ///////////////////////////////////////
 // Selecting, Creating, and Deleting Elements
